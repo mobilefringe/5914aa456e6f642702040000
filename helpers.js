b037b91e1229b9acc29b2f6db638ffda996d305e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-11 16:46:06 -0400

    South Oakville Centre 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -26,32 +26,6 @@ function init(e){
 	$('.accordion_header').click(function(e){
         $(this).find('i').toggleClass('fa-caret-down fa-caret-up');
 	});
-	
-    get_instagram("//dixieoutlet.mallmaverick.com/api/v2/dixieoutlet/social.json", 9, 'thumbnail', render_instagram)
-    
-    blog_searcher();
-    
-    $('#subForm').submit(function(e){
-        e.preventDefault();
-        if($("#agree_newsletter").is(':checked')){
-            $.getJSON(
-                this.action + "?callback=?",
-                $(this).serialize(),
-                function (data) {
-                    if (data.Status === 400) {
-                        alert("An error occured while processing your request. Please try again later.");
-                    } else { // 200
-                        $("#subscription_confirmed").fadeIn();
-                        $('#subscription_confirmed').delay(2000).fadeOut();
-                        $('#subForm').trigger('reset');
-                    }
-            });
-        }
-        else{
-            $("#agree_newsletter").focus();
-            alert("Please agree to receive newsletter before continuing.")
-        }
-    });
 }
 function show_content(){
     $('.yield').fadeIn();
