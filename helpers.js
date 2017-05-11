29d5ae1ffa645b20b6f7a8c68e96df6ba24aa628
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-11 16:47:52 -0400

    South Oakville Centre 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -35,5 +35,5 @@ function show_content(){
          $('.panel-collapse').removeClass('in')
     }
     var today_hours = getTodaysHours();
-    renderHomeHours('#home_hours_container', '#home_hours_template', today_hours)
+    //renderHomeHours('#home_hours_container', '#home_hours_template', today_hours)
 }
