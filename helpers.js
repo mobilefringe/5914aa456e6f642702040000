589e215eba4a62544141fef8751234e2f991219a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-11 16:56:30 -0400

    South Oakville Centre 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -1,15 +1,6 @@
 function init(e){
     $('<div class="modal-backdrop custom_backdrop"><img src="//kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
     
-    $('.menu_toggler').click(function(){
-        $('#navbar').slideToggle();
-        if ($('.menu_toggler').attr('src') == '//codecloud.cdn.speedyrails.net/sites/562a661e6e6f64426b000000/image/png/1445970905000/menu.png'){
-            $('.menu_toggler').attr('src', '//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/png/1452534608000/menu_close.png');
-        }else{
-            $('.menu_toggler').attr('src', '//codecloud.cdn.speedyrails.net/sites/562a661e6e6f64426b000000/image/png/1445970905000/menu.png');
-        }
-    })
-    
     $(window).scroll(function(e){
 		if ($(this).scrollTop() > 100) {
 			$('.scrollToTop').fadeIn();
