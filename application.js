1094d31b65a96a1b54d6236d5606115aeb25e302
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-15 17:12:37 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -65,6 +65,19 @@ function renderGeneral(container, template, collection){
     $(container).html(item_rendered.join(''));
 }
 
+function renderPropertyDetails(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        var repo_rendered = Mustache.render(template_html,val);
+        item_rendered.push(repo_rendered);
+        console.log(val);
+    });
+    $(container).html(item_rendered.join(''));
+}
+
 function renderStoreList(container, template, collection, type){
     var item_list = [];
     var item_rendered = [];
