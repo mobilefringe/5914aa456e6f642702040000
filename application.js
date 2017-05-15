a3ea74061fe985b5084c53a0ab3d3a32d153ece0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-15 17:14:46 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -71,9 +71,9 @@ function renderPropertyDetails(container, template, collection){
     var template_html = $(template).html();
     Mustache.parse(template_html); 
     $.each( collection , function( key, val ) {
-         var new_val.id = val.id;
+         var new_val;
         if(key == "name") {
-            val.name = 
+            new_val.name = 
         }
         var repo_rendered = Mustache.render(template_html,val);
         item_rendered.push(repo_rendered);
