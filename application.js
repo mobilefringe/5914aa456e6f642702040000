473b193a1ad9df666c3b6c23381ef4017e8d0287
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-15 14:12:47 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -641,7 +641,7 @@ function renderHomeDropdownHours(container, template, collection, type){
     var item_list = [];
     var item_rendered = [];
     var today_hours = getTodaysHours();
-    console.log(today_hours[0]);
+    console.log(today_hours);
     var template_html = $(template).html();
     
     Mustache.parse(template_html);   // optional, speeds up future uses
