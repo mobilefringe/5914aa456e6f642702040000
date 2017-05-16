52d6053409caaab2f9c66af1bdc390317b8d3b0e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-16 13:15:20 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -461,7 +461,7 @@ function renderPromotions(container, template, collection){
         }
         
         if(val.image_url.indexOf('missing.png') > 0){
-            val.image_url  = "//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/jpeg/1446753494000/Dixie_default.jpg";;
+            val.image_url  = "//codecloud.cdn.speedyrails.net/sites/5914aa456e6f642702040000/image/png/1494532252000/logo_hopedale.png";;
         }
         
         var show_date = moment(val.show_on_web_date);
