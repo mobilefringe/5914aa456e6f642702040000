56674a31f434cf8d900d0fac8a3cf4416cfa03b2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-16 13:14:33 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -406,7 +406,7 @@ function renderJobDetails(container, template, collection){
             }
         }
         else{
-            val.store_name = "Dixie Outlet";
+            val.store_name = "South Oakville Centre";
             val.image_url = "//codecloud.cdn.speedyrails.net/sites/5914aa456e6f642702040000/image/png/1494532252000/logo_hopedale.png";
         }
         
