37bf11ab72da89850a3cce9e105ec961b24c4e56
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-16 13:13:43 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -493,7 +493,7 @@ function renderPromoDetails(container, template, collection){
             val.store_detail_btn = store_details.slug;
             val.store_name = store_details.name;
             if (store_details.store_front_url_abs.indexOf('missing.png') > -1){
-                val.image_url = "//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/jpeg/1446753494000/Dixie_default.jpg";
+                val.image_url = "//codecloud.cdn.speedyrails.net/sites/5914aa456e6f642702040000/image/png/1494532252000/logo_hopedale.png";
             }
             else{
                 val.image_url = store_details.store_front_url_abs;
@@ -501,7 +501,7 @@ function renderPromoDetails(container, template, collection){
         }
         else{
             val.store_name = "South Oakville Centre";
-            val.image_url = "//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/jpeg/1446753494000/Dixie_default.jpg";
+            val.image_url = "//codecloud.cdn.speedyrails.net/sites/5914aa456e6f642702040000/image/png/1494532252000/logo_hopedale.png";
         }
         
         if(val.promo_image_url_abs.indexOf('missing.png') > -1){
