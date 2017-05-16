e39a9284314db2916a3e7510eb1b8c47c798cac8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-16 13:13:17 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -456,7 +456,7 @@ function renderPromotions(container, template, collection){
             val.image_url = store_details.store_front_url_abs;
         }
         else{
-            val.store_name = "Dixie Outlet";
+            val.store_name = "South Oakville Centre";
             val.image_url = "//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/jpeg/1446753494000/Dixie_default.jpg";
         }
         
