ba71448685351b8c39f458b5f884ddec6af8d0c3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-17 11:10:15 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -500,12 +500,11 @@ function renderPromoDetails(container, template, collection){
             if(val.image_url.indexOf('missing.png') > 0){
                 if (store_details.store_front_url_abs.indexOf('missing.png') > -1){
                 val.image_url = "//codecloud.cdn.speedyrails.net/sites/5914aa456e6f642702040000/image/png/1494532252000/logo_hopedale.png";
+                }
+                else{
+                    val.image_url = store_details.store_front_url_abs;
+                }
             }
-            else{
-                val.image_url = store_details.store_front_url_abs;
-            }
-            }
-            
         }
         else{
             val.store_name = "South Oakville Centre";
