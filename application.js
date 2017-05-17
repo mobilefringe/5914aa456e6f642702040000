5a0ade3c7dd5e403fad8ceceb3f2dd82ce0d1585
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-17 11:03:30 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -453,6 +453,7 @@ function renderPromotions(container, template, collection){
             var store_details = getStoreDetailsByID(val.promotionable_id);
             val.store_detail_btn = store_details.slug ;
             val.store_name = store_details.name;
+            val.image_url  = val.promo_image_url_abs;
             if(val.image_url.indexOf('missing.png') > 0){
                 val.image_url = store_details.store_front_url_abs;
             }
