8286569c7d9f7597e8961b01229b9e9e1f28f4d7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-17 16:43:42 -0400

    South Oakville Centre 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -19,7 +19,7 @@ function init(e){
 	});
 	
 	$('.dropdown_btn').click(function(e){
-	    $("#hours_container").toggle(false);
+	    $("#hours_container").toggle();
 	    console.log("toggle");
 	});
 }
