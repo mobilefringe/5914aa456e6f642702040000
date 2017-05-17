5e54c45c8e2f5dd4c62f2a78bb7b471b570ed063
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-17 17:02:44 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -330,10 +330,7 @@ function renderHours(container, template, collection, type){
     
     if (type == "holiday_hours") {
         $.each( collection , function( key, val ) {
-             if(item_list.length < 0)
-             {
-                  val.display_style ="display:none";
-             }
+             
             if (!val.store_id && val.is_holiday == true) {
                 holiday = moment(val.holiday_date);
                 val.formatted_date = in_my_time_zone(holiday, "MMM D");
