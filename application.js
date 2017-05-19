495db89bb0cbde4429dae859e05c828931dbbb62
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-19 15:39:27 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -85,6 +85,10 @@ function renderVendorScroll(container, template, collection){
     });
     
     var items_length = item_list.length;
+    for (var i = 0; i < 3; i ++)
+    {
+        
+    }
     $.each( item_list , function( key, val ) {
         var repo_rendered = Mustache.render(template_html,val);
         item_rendered.push(repo_rendered);
