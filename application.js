8274a5a523d14878eed4068b037fa357b1d74693
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-12 10:54:50 -0400

    South Oakville Centre 1.0  /application.js

--- application.js
+++ application.js
@@ -316,7 +316,7 @@ function renderStoreDetails(container, template, collection, slug){
         else{
             val.facebook_show = "display:none";
         }
-        if((val.website== null || val.website == "")){
+        if((val.website === null || val.website === "")){
         
         }
         else {
