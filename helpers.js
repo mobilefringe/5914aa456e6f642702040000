d7ba5f642877c3925ad0ff30684612714cb5768d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-09-15 09:42:25 -0400

    South Oakville Centre 1.1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -21,7 +21,7 @@ function init(e){
 	var today_hours = getTodaysHours();
     var hours = getPropertyRegularHours();
     renderHomeHours('#homem_hours_container', '#homem_hours_template', today_hours);
-    renderHomeDropdownHours('#menu_hours_container', '#menu_hours_template', hours, 'reg_hours');
+    renderHomeDropdownHours('#hours_container', '#hours_template', hours, 'reg_hours');
 }
 
 function show_content(){
