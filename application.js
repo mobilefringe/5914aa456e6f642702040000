4b8fb1b43dfc9f9320a09b1fc9e3a21881a275f3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-09-15 10:07:26 -0400

    South Oakville Centre 1.1  /application.js

--- application.js
+++ application.js
@@ -741,7 +741,7 @@ function renderHomeDropdownHours(container, template, collection){
     var today_hours = getTodaysHours().day_of_week;
     var template_html = $(template).html();
     Mustache.parse(template_html);   // optional, speeds up future uses
-    if (type == "reg_hours") {
+    // if (type == "reg_hours") {
         $.each( collection , function( key, val ) {
             if (!val.store_id && val.is_holiday == false) {
                 switch(val.day_of_week) {
@@ -785,7 +785,7 @@ function renderHomeDropdownHours(container, template, collection){
         });
         collection = [];
         collection = item_list;
-    }
+    // }
     
     $.each( collection , function( key, val ) {
         var rendered = Mustache.render(template_html,val);
